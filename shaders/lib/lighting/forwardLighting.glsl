e7e2690b6290dd6fbfeaa760cb680aa3fb87c7ec
Loui Bénas <loui.benas@web.de>
2022-07-12 20:22:04 +0200

    New minlight amount

--- shaders/lib/lighting/forwardLighting.glsl
+++ shaders/lib/lighting/forwardLighting.glsl
@@ -95,7 +95,7 @@ void GetLighting(
     float newLightmap  = pow(lightmap.x, 10.0) * (EMISSIVE_BRIGHTNESS + 0.5) + lightmap.x * 0.7;
     vec3 blockLighting = blocklightCol * newLightmap * newLightmap;
 
-    float minLighting = (0.009 * screenBrightness + 0.001) * (1.0 - eBS);
+    float minLighting = 0.04 * (1.0 - eBS);
 
     #ifdef TOON_LIGHTMAP
     minLighting *= floor(smoothLighting * 8.0 + 1.001) / 4.0;
@@ -106,7 +106,7 @@ void GetLighting(
 
     float nightVisionLighting = nightVision * 0.25;
     
-    albedo *= sceneLighting + blockLighting + emissiveLighting + nightVisionLighting + minLighting;
+    albedo *= max(sceneLighting + blockLighting + emissiveLighting + nightVisionLighting, minLighting);
     albedo *= quarterNdotU * smoothLighting * smoothLighting;
 
     #ifdef DESATURATION
