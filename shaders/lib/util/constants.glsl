6c780d9bc664cec723f77955ef34825b79a0fe66
Loui Bénas <loui.benas@web.de>
2022-07-26 17:24:03 +0200

    Increase hardware epsilon

--- shaders/lib/util/constants.glsl
+++ shaders/lib/util/constants.glsl
@@ -11,4 +11,4 @@ const float PI  = 3.14159265359;
 const float TAU = 6.28318530718;
 const float PHI = 2.39996322973;
 const float INV_PI = 0.318309;
-const float EPS = 1e-10;
+const float EPS = 1e-5;
