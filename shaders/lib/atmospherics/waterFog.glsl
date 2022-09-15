088615671af0bd8cfa227c0b9f15c782a18d8740
Loui Bénas <loui.benas@web.de>
2022-09-16 01:07:13 +0200

    Tweaked Water Fog

--- shaders/lib/atmospherics/waterFog.glsl
+++ shaders/lib/atmospherics/waterFog.glsl
@@ -9,6 +9,6 @@ See AGREEMENT.txt for more information.
 void WaterFog(inout vec3 color, vec3 viewPos, float fogrange)
 {
     float fog = length(viewPos) / fogrange;
-    fog = 1.0 - exp(-3.0 * fog * fog);
-    color = mix(color, Pow2(waterColor.rgb * (1.0 - blindFactor)), fog);
+    fog = 1.0 - exp(-3.0 * fog);
+    color = mix(color, Pow2(Lift(waterColor.rgb, -0.06) * (1.0 - blindFactor)), fog * 0.9);
 }
