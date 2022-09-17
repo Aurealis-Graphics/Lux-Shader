7ee69bd1a4b8d612020e68c94f627333a3e96ce1
Loui Bénas <loui.benas@web.de>
2022-09-17 22:06:39 +0200

    End Sky Optimizations

--- shaders/lib/atmospherics/endSky.glsl
+++ shaders/lib/atmospherics/endSky.glsl
@@ -9,44 +9,40 @@ See AGREEMENT.txt for more information.
 const float gravitationalConst = 9.81;
 vec3 GetWaveLayer(vec2 coord, float wavelength, float steepness, float time, float speed, vec2 direction) 
 {
-    vec3 gerstner;
-    float k = 2.0 * PI / wavelength;
-    float c = sqrt(gravitationalConst / k);
+    float k = TAU / wavelength;
     float a = steepness / k;
-    vec2 dir = normalize(direction);
-    float f = k * (dot(dir, coord.xy) - c * time * speed);
+    vec2 dir = direction;
+    float f = k * (dot(dir, coord.xy) - Lift(k, 1.6) * time * speed);
     
-    gerstner.x += dir.x * (a * cos(f)) / a;
-    gerstner.y = sin(f);
-    gerstner.z += dir.y * (a * cos(f)) / a;
+    vec3 layer;
+    layer.xz += dir.xy * (a * cos(f)) / a;
+    layer.y = sin(f);
     
-    return gerstner;
+    return layer;
 }
 
+const float endSkyPersistance = 0.99;
+const float endSkyLacunarity = 1.2;
 vec3 GetEndSkyColor(vec3 viewPos) 
 {
     vec3 viewDir = mat3(gbufferModelViewInverse) * normalize(viewPos);
-    vec2 coord = viewDir.xz / (1.0 + abs(viewDir.y));
-    coord *= 80.0;
+    vec2 coord = viewDir.xz / (1.0 + abs(viewDir.y)) * 80.0;
 
     vec3 pattern = vec3(0.0);
-    
-    const float persistance = 0.99;
-    const float lacunarity = 1.2;
+
     float amplitude = 1.0;
     float frequency = 1.0;
 
     for (int i = 0; i < 16; i++)
     {
-        vec2 direction = vec2(1.0) * Rotate(float(i) * 4.3333);
-        vec3 waveLayer = GetWaveLayer(coord, 20.0 / frequency, amplitude, frameTimeCounter, 0.1, direction);
+        vec2 direction = vec2(0.707106782) * Rotate(float(i) * 4.3333);
+        vec3 waveLayer = GetWaveLayer(coord, 20.0 / frequency, amplitude, frameTimeCounter, 0.31321, direction);
         
         pattern += waveLayer.y * 0.5 + 0.5;
-
         coord -= waveLayer.xz * 0.36;
 
-        amplitude *= persistance;
-        frequency *= lacunarity;
+        amplitude *= endSkyPersistance;
+        frequency *= endSkyLacunarity;
     }
 
     pattern = Saturate(pattern * 0.05);
